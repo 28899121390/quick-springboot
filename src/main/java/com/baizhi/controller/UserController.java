cbfc27bb97f44899a7a46f028a33f0ede8a8d15f
htf <2889921390@qq.com>
2018-11-25 14:47:39 +0800

    second project

--- src/main/java/com/baizhi/controller/UserController.java
+++ src/main/java/com/baizhi/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("test")
 public class UserController {
+
     @RequestMapping("test")
     public String test() {
         return "index";
