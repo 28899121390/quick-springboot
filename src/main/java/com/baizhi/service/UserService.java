ee3d972d664d834329a9897b77e4f2ab4ded2d77
lx <2377769679@qq.com>
2018-11-25 15:30:12 +0800

    seven commit

--- src/main/java/com/baizhi/service/UserService.java
+++ src/main/java/com/baizhi/service/UserService.java
@@ -3,6 +3,7 @@ package com.baizhi.service;
 public class UserService {
     public static void main(String[] args) {
         System.out.println("我是理想");
+        System.out.println("我是理想plus");
 
     }
 }
